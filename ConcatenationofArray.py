f6c394ed55df1e63a631734ba30ea9d81afcc0cc
Sai Nishwanth <sainishwanthraj@gmail.com>
2023-02-19 15:01:17 +0530

    19-02-2023

--- ConcatenationofArray.py
+++
@@ -1,8 +0,0 @@
-def getConcatenation(nums: list[int]) -> list[int]:
-        lst = [i for i in nums]
-        print(lst)
-        for j in range(len(lst)):
-            lst.append(lst[j])
-        return lst
-
-print(getConcatenation([1,2,3,4]))
